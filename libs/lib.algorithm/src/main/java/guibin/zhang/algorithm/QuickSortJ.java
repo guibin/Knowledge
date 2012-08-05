4c06e368ec07ba9d598fe2b0a1a6020a4290f321
Guibin Zhang <guibin.beijing@gmail.com>
2012-08-05 13:09:05 +0800

    Add QuickSort Java version

---
+++ libs/lib.algorithm/src/main/java/guibin/zhang/algorithm/QuickSortJ.java
@@ -0,0 +1,130 @@
+package guibin.zhang.algorithm;
+
+/**
+ * The basic concept is to pick one of the elements in the array as a pivot
+ * value around which the other elements will be rearranged. Everything less
+ * than the pivot is moved left of the pivot (into the left partition).
+ * Similarly, everything greater than the pivot goes into the right partition.
+ * At this point each partition is recursively quicksorted.
+ *
+ * The Quicksort algorithm is fastest when the median of the array is chosen as
+ * the pivot value. That is because the resulting partitions are of very similar
+ * size. Each partition splits itself in two and thus the base case is reached
+ * very quickly.
+ *
+ * In practice, the Quicksort algorithm becomes very slow when the array passed
+ * to it is already close to being sorted. Because there is no efficient way for
+ * the computer to find the median element to use as the pivot, the first
+ * element of the array is used as the pivot. So when the array is almost
+ * sorted, Quicksort doesn't partition it equally. This means that one of the
+ * recursion branches is much deeper than the other, and causes execution time
+ * to go up. Thus, it is said that the more random the arrangement of the array,
+ * the faster the Quicksort Algorithm finishes.
+ *
+ * @author guibin
+ */
+public class QuickSortJ {
+
+    //http://www.mycstutorials.com/articles/sorting/quicksort
+    public void doSort(int[] array, int start, int end) {
+        int i = start;
+        int k = end;
+
+        // check that there are at least two elements to sort
+        if (end - start >= 1) {
+            // set the pivot as the first element in the partition
+            int pivot = array[start];
+
+            while (k > i) { // while the scan indices from left and right have not met,
+                // From the left find the first element great than the pivot.
+                while (array[i] <= pivot && i < end) {
+                    i++;
+                }
+                // From the right find the first element not greater than the pivot.
+                while (array[k] > pivot && k > start) {
+                    k--;
+                }
+                // If the left index is still smaller than the right index, 
+                // swap the corresponding elements
+                if (k > i) {
+                    swap(array, i, k);
+                }
+            }
+            // after the indices have crossed, 
+            // swap the last element in the left partition with the pivot 
+            swap(array, start, k);
+            // Now the pivot is at position k
+
+            //Sort the left partition
+            doSort(array, start, k - 1);
+            //Sort the right partition
+            doSort(array, k + 1, end);
+        }
+    }
+
+    /**
+     * It partitions the portion of the array between indexes left and right,
+     * inclusively, by moving all elements less than array[pivotIndex] before
+     * the pivot, and the equal or greater elements after it.
+     *
+     * @param array
+     * @param left left is the index of the leftmost element of the array
+     * @param right right is the index of the rightmost element of the array
+     * (inclusive) number of elements in subarray = right-left+1
+     * @param pivotIndex
+     * @return
+     */
+    public int partition(int[] array, int left, int right, int pivotIndex) {
+        int pivotValue = array[pivotIndex];
+        // Move pivot to end
+        swap(array, pivotIndex, right);
+        int storeIndex = left;
+
+        for (int i = left; i < right; i++) {
+            if (array[i] < pivotValue) {
+                swap(array, i, storeIndex);
+                storeIndex++;
+            }
+        }
+        // Move pivot to its final place
+        swap(array, storeIndex, right);
+
+        return storeIndex;
+    }
+
+    public void doSortInplaceVersion(int[] array, int start, int end) {
+
+        if (start < end) {
+            //choose any 'pivotIndex' such that 'left' ≤ 'pivotIndex' ≤ 'right'
+            int pivotIndex = start;
+
+            // Get lists of bigger and smaller items and final position of pivot
+            int pivotIndexNew = partition(array, start, end, pivotIndex);
+            doSortInplaceVersion(array, start, pivotIndexNew - 1);
+            doSortInplaceVersion(array, pivotIndexNew + 1, end);
+        }
+    }
+
+    private void swap(int[] array, int i, int k) {
+        int temp = array[i];
+        array[i] = array[k];
+        array[k] = temp;
+    }
+
+    private void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ",");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        QuickSortJ qs = new QuickSortJ();
+
+        int[] array = {3, 1, 4, 5, 9, 2, 6, 8, 7};
+//        int[] array = {1,2,3,4,5,6,7,10,8};
+//        qs.doSort(array, 0, array.length - 1);
+        qs.doSortInplaceVersion(array, 0, array.length - 1);
+        qs.printArray(array);
+    }
+}
--- libs/lib.algorithm/src/main/scala/guibin/zhang/algorithm/QuickSortS.scala
+++ libs/lib.algorithm/src/main/scala/guibin/zhang/algorithm/QuickSortS.scala
@@ -2,7 +2,7 @@ package guibin.zhang.algorithm
 
 import scala.collection.mutable.ListBuffer
 
-object QuickSort {
+object QuickSortS {
   
   def main(args: Array[String]) {
     val list = ListBuffer(20, 19, 35, 44, 17, 22, 1, 2, 78)
@@ -10,7 +10,17 @@ object QuickSort {
     println(list.mkString(",") + " => " + result.mkString(","))
   }
   
-  
+  /**
+   * The simple version:
+   * 1. Pick an element, called a pivot, from the list. We pick the first one of the list.
+   * 2. Select and remove a pivot value(head) from the list
+   * 3. Create empty lists 'less' and 'greater'
+   * 4. Traverse the list, if 'x' ≤ 'pivot' then append 'x' to 'less', else append 'x' to 'greater'
+   * 5. return concatenate(quicksort('less'), 'pivot', quicksort('greater')) 
+   * 
+   * Disadvantage of this simple version:
+   * 1. it requires O(n) extra storage space, which is as bad as MergeSort.
+   */
   def simpleVersion(list: ListBuffer[Int]): ListBuffer[Int] = {
     if(list.size <= 1) {
       return list //a list of zero or one element is already sorted.
