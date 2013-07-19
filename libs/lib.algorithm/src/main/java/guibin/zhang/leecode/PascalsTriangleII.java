576bf750968293caa0b822a18128961677cf01e2
Guibin Zhang <guibin.beijing@gmail.com>
2013-07-19 21:17:31 +0800

    Add problem statement

--- libs/lib.algorithm/src/main/java/guibin/zhang/leecode/PascalsTriangleII.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leecode/PascalsTriangleII.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 
 /**
  *
+ * Given an index k, return the kth row of the Pascal's triangle.
+ * 
+ * For example, given k = 3,
+ * Return [1,3,3,1].
+ * 
+ * Note:
+ * 
+ * Could you optimize your algorithm to use only O(k) extra space?
+ * 
  * @author Gubin Zhang <guibin.beijing@gmail.com>
  * 
  * http://blog.csdn.net/guixunlong/article/details/8851137
