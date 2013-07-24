7deae480ed0535a461febe3cb1e85fc1322a78bc
Guibin Zhang <guibin.beijing@gmail.com>
2013-07-24 17:08:23 +0800

    Add Maximum Depth of Binary Tree

---
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leecode/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,35 @@
+package guibin.zhang.leecode;
+
+/**
+ *
+ * Given a binary tree, find its maximum depth.
+ * 
+ * The maximum depth is the number of nodes along the longest path 
+ * from the root node down to the farthest leaf node.
+ * 
+ * Run Status: Accepted!
+ * Program Runtime: 700 milli secs
+ * Progress: 38/38 test cases passed.
+ * 
+ * @author Gubin Zhang <guibin.beijing@gmail.com>
+ */
+public class MaximumDepthOfBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    
+    public int maxDepth(TreeNode root) {
+        
+        if(root == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        
+    }
+}
