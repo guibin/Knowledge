a1e86b4ec6254232e73bdf82794f0981b25587a4
Guibin Zhang <guibin.beijing@gmail.com>
2013-08-01 15:39:44 +0800

    Add Gray Code

---
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leecode/GrayCode.java
@@ -0,0 +1,90 @@
+package guibin.zhang.leecode;
+
+import java.util.ArrayList;
+
+/**
+ * 
+ * The gray code is a binary numeral system where two successive values differ in only one bit.
+ * 
+ * Given a non-negative integer n representing the total number of bits in the code, 
+ *   print the sequence of gray code. A gray code sequence must begin with 0.
+ * 
+ * For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+ * 
+ * 00 - 0
+ * 01 - 1
+ * 11 - 3
+ * 10 - 2
+ * Note:
+ * For a given n, a gray code sequence is not uniquely defined.
+ * 
+ * For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+ * 
+ * For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+ * 
+ * @author Gubin Zhang <guibin.beijing@gmail.com>
+ */
+public class GrayCode {
+    public ArrayList<Integer> grayCode(int n) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if(n == 0) {
+            result.add(0);
+            return result;
+        } else if(n == 1) {
+            result.add(0);
+            result.add(1);
+            return result;
+        }
+        
+        int num = (int)Math.pow(2, n) - 1;
+        result.add(num);
+        for(int i = n; i >0; i --) {
+            num = num >> 1;
+            result.add(num);
+        }
+        num = (int)Math.pow(2, n - 1);
+        result.add(num);
+        for(int j = n; j > 2; j--) {
+            num += num >> 1;
+            result.add(num);
+        }
+        
+        return result;
+    }
+    
+    /**
+     * http://fisherlei.blogspot.com/2012/12/leetcode-gray-code.html
+     * http://baike.baidu.com/view/358724.htm
+     * @param n
+     * @return 
+     */
+    public ArrayList<Integer> grayCode_v2(int n) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        if (n == 0) {
+            result.add(0);
+            return result;
+        };
+        result.add(0);
+        result.add(1);
+        for (int i = 1; i < n; i++) {
+            ArrayList<Integer> tmp = new ArrayList<Integer>(result);
+            Integer a = 1 << i;
+            for (int k = result.size() - 1; k >= 0; k--) {
+                tmp.add(result.get(k) + a);
+            }
+            result = tmp;
+        }
+        return result;
+    }
+    
+    public static void main(String[] args) {
+        GrayCode gc = new GrayCode();
+        ArrayList<Integer> result = gc.grayCode_v2(5);
+        int j = 0;
+        for(int i : result) {
+            System.out.println((j++) + "\t" + Integer.toBinaryString(i) + "\t" + i );
+        }
+        System.out.println();
+        System.out.println("n = " + 5);
+    }
+}
