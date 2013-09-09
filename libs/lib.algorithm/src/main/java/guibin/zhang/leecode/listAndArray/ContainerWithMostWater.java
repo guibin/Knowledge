5681560629620f3068c227ab848cc3d5719d5b1d
Guibin Zhang <guibin.beijing@gmail.com>
2013-09-08 22:31:26 -0700

    Add Container with Most Water

---
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leecode/listAndArray/ContainerWithMostWater.java
@@ -0,0 +1,37 @@
+package guibin.zhang.leecode.listAndArray;
+
+/**
+ *
+ * Given n non-negative integers a1, a2, ..., an, 
+ * where each represents a point at coordinate (i, ai). 
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). 
+ * Find two lines, which together with x-axis forms a container, 
+ * such that the container contains the most water.
+ * 
+ * Note: You may not slant the container.
+ * 
+ * The thought is very similar with the **FourSum, ThreeSum**.
+ * 
+ * @author Gubin Zhang <guibin.beijing@gmail.com>
+ */
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        int len = height.length;
+        int start = 0;
+        int end = len - 1;
+        int maxArea = 0;
+        
+        while(start < end) {
+            int area = (end - start) * Math.min(height[start], height[end]);
+            maxArea = Math.max(area, maxArea);
+            if (height[start] < height[end]) {
+                start ++;
+            } else {
+                end --;
+            }
+        }
+        return maxArea;
+    }
+}
