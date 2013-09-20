43e9f5f190ceed7098ceb33aa1aee8f9232cfe19
Guibin Zhang <guibin.beijing@gmail.com>
2013-09-19 22:26:07 -0700

    Add more solution for Add Two Numbers

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/AddTwoNumbers.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/AddTwoNumbers.java
@@ -78,6 +78,46 @@ public class AddTwoNumbers {
         return dummy.next;
     }
     
+    public ListNode addTwoNumbers_v2(ListNode l1, ListNode l2) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        int sum = 0;
+        while(l1 != null && l2 != null) {
+            sum = l1.val + l2.val + carry;
+            ListNode node = new ListNode(sum % 10);
+            curr.next = node;
+            carry = sum / 10;
+            l1 = l1.next;
+            l2 = l2.next;
+            curr = curr.next;
+        }
+        
+        while (l1 != null) {
+            sum = l1.val + carry;
+            ListNode node = new ListNode(sum % 10);
+            curr.next = node;
+            carry = sum / 10;
+            curr = curr.next;
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            sum = l2.val + carry;
+            ListNode node = new ListNode(sum % 10);
+            curr.next = node;
+            carry = sum / 10;
+            curr = curr.next;
+            l2 = l2.next;
+        }
+        if (carry > 0) {
+            ListNode node = new ListNode(carry);
+            curr.next = node;
+        }
+        return dummy.next;
+    }
+    
     public static void main(String[] args) {
         AddTwoNumbers at = new AddTwoNumbers();
         ListNode a = at.new ListNode(1);
