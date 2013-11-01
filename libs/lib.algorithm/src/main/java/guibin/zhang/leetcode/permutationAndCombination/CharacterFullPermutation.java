08f6a0789cd44892b9f99e4ef601ed2888e889b4
Guibin Zhang <guibin.beijing@gmail.com>
2013-11-01 16:18:24 -0700

    Make the uniform style with CharacterCombination.

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/permutationAndCombination/CharacterFullPermutation.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/permutationAndCombination/CharacterFullPermutation.java
@@ -14,22 +14,22 @@ public class CharacterFullPermutation {
      * 
      * @param arr The source array.
      * @param branch The target array, which contains the permutation result.
-     * @param level The index for target array.
+     * @param idx The index for target array.
      * @param visited The array which is used to track the visiting status.
      */
-    public void generatePermutations(char[] arr, char[] branch, int level, boolean[] visited) {
+    public void generatePermutations(char[] arr, char[] branch, int idx, boolean[] visited) {
         
-        if (level >= arr.length - 1) {
+        if (idx == arr.length) {
             System.out.println(branch);
             return;
         }
         for (int i = 0; i < arr.length; i++) {
             if (!visited[i]) {
-                branch[++level] = arr[i];
+                branch[idx++] = arr[i];
                 visited[i] = true;
-                generatePermutations(arr, branch, level, visited);
+                generatePermutations(arr, branch, idx, visited);
                 visited[i] = false;
-                level --;
+                idx --;
             }
         }
     }
@@ -43,7 +43,7 @@ public class CharacterFullPermutation {
         char[] arr = str.toCharArray();
         boolean[] visited = new boolean[n];
         char[] branch = new char[n];
-        cp.generatePermutations(arr, branch, -1, visited);
+        cp.generatePermutations(arr, branch, 0, visited);
         
         /**
          * The expected permutation ABCD is:
