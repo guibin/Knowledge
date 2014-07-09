59900eae9496a91eb161edbe609b417b134ad524
Guibin Zhang <guibin.beijing@gmail.com>
2014-07-09 00:12:05 -0700

    Add partition and quick sort

---
+++ libs/lib.algorithm/src/main/java/guibin/zhang/onlinecourse/QuickSort.java
@@ -0,0 +1,107 @@
+package guibin.zhang.onlinecourse;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * 
+ * https://d396qusza40orc.cloudfront.net/algs4partI/slides/23Quicksort.pdf
+ * 
+ * @author Guibin Zhang <guibin.zhang@gmail.com>
+ */
+public class QuickSort {
+    
+    /**
+     * Partition the array of a from lo(inclusive) to hi(inclusive) using a[lo] as pivot.
+     * @param a Array to be partitioned.
+     * @param lo The low index inclusive. Choose a[lo] as the partitioning element(pivot).
+     * @param hi The high index inclusive.
+     * @return The index j of the array a so that no larger entry to the left of j, 
+     *         and no smaller entry to the right of j. And a[j] == a[lo]
+     */
+    public int partition2(Comparable[] a, int lo, int hi) {
+        
+        int i = lo + 1;
+        int j = hi;
+        while (true) {
+            //Scan from lo to hi to reach the first element that is greater than pivot
+            while (i < hi && a[i].compareTo(a[lo]) <= 0) {
+                i++;
+            }
+            //Scan from hi to lo to reach the first element that is less than pivot
+            while (j > lo && a[j].compareTo(a[lo]) >= 0) {
+                j--;
+            }
+            
+            if (i >= j) break;
+            
+            swap(a, i, j);
+        }
+        swap(a, lo, j);
+        
+        return j;
+    }
+    
+    public int partition(Comparable[] a, int lo, int hi) {
+        
+        int i = lo;
+        int j = hi + 1;
+        while (true) {
+            //Scan from lo to hi to reach the first element that is greater than pivot
+            while (a[++i].compareTo(a[lo]) <= 0) {
+                if (i == hi) break;
+            }
+            //Scan from hi to lo to reach the first element that is less than pivot
+            while (a[--j].compareTo(a[lo]) >= 0) {
+                if (j == lo) break;
+            }
+            
+            if (i >= j) break;
+            
+            swap(a, i, j);
+        }
+        swap(a, lo, j);
+        
+        return j;
+    }
+    
+    public void sort(Comparable[] a, int lo, int hi) {
+        
+        if (hi <= lo) return;
+        
+        int j = partition(a, lo, hi);
+        sort(a, lo, j - 1);
+        sort(a, j + 1, hi);
+    }
+    
+    private void swap(Comparable[] a, int i, int j) {
+        Comparable tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    
+    public static void main(String[] args) {
+//        Random rd = new Random();
+//        Comparable[] a = new Comparable[10];
+//        System.out.println("Target array: ");
+//        for (int i = 0; i < 10; i++) {
+//            a[i] = rd.nextInt(1000);
+//            System.out.print(a[i] + ",");
+//        }
+//        System.out.println();
+        
+        Comparable[] a = {50, 61, 44, 32, 99, 87, 51, 50, 50, 12};
+        QuickSort s = new QuickSort();
+        int j = s.partition2(a, 0, a.length - 1);
+        System.out.println("The partition index is " + j);
+        System.out.println("After partition, the array becomes: ");
+        Arrays.stream(a).forEach((c) -> System.out.print(c + ","));
+        
+        System.out.println();
+        System.out.println("After sorting");
+        s.sort(a, 0, a.length - 1);
+        Arrays.stream(a).forEach((c) -> {System.out.print(c + ",");});
+        System.out.println();
+        
+    }
+}
