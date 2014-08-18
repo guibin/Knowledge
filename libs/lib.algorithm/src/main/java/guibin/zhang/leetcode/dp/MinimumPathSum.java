88184c03fb6bddb55b3fb3f960dca716988a7b08
Guibin Zhang <gzhang@radiumone.com>
2014-08-17 23:22:49 -0700

    Add minimum path cost

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/dp/MinimumPathSum.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/dp/MinimumPathSum.java
@@ -38,4 +38,82 @@ public class MinimumPathSum {
         }
         return p[rows - 1][cols - 1];
     }
+    
+    /**
+     * Question link: http://www.geeksforgeeks.org/dynamic-programming-set-6-min-cost-path/
+     * 
+     * Given a cost matrix cost[][] and a position (m, n) in cost[][], 
+     * write a function that returns cost of minimum cost path to reach (m, n) from (0, 0). 
+     * Each cell of the matrix represents a cost to traverse through that cell. 
+     * Total cost of a path to reach (m, n) is sum of all the costs on that path 
+     * (including both source and destination). 
+     * 
+     * You can only traverse down, right and **diagonally** lower cells from a given cell, 
+     * i.e., from a given cell (i, j), cells (i+1, j), (i, j+1) and (i+1, j+1) can be traversed. 
+     * You may assume that all costs are positive integers.
+     * 
+     * This question is different from above minPathSum, since this question allow move diagonally.
+     * 
+     * Idea:
+     * The path to reach (m, n) must be through one of the 3 cells: 
+     * (m-1, n-1) or (m-1, n) or (m, n-1). 
+     * 
+     * So minimum cost to reach (m, n) can be written as “minimum of the 3 cells plus cost[m][n]“.
+     * 
+     * minCost(m, n) = min (minCost(m-1, n-1), minCost(m-1, n), minCost(m, n-1)) + cost[m][n]
+     * 
+     * @param cost The cost matrix
+     * @param m end x
+     * @param n end y
+     * @return min cost from (0, 0) to (m, n)
+     */
+    public int minCostPathNaive(int[][] cost, int m, int n) {
+
+        if (m < 0 || n < 0) {
+            return Integer.MAX_VALUE;
+        } else if (m == 0 && n == 0) {
+            return cost[0][0];
+        } else {
+            return cost[m][n] + min(minCostPathNaive(cost, m - 1, n - 1),
+                    minCostPathNaive(cost, m, n - 1),
+                    minCostPathNaive(cost, m - 1, n));
+        }
+    }
+    
+    private int min(int x, int y, int z) {
+        return Math.min(Math.min(x, y), z);
+    }
+    
+    public int minCostPath(int[][] cost, int m, int n) {
+        
+        int[][] minCost = new int[cost.length][cost[0].length];
+        
+        //Base case
+        minCost[0][0] = cost[0][0];
+        //The first colomn case, each cell can only be computed from previous one.
+        for (int i = 1; i < cost.length; i++) {
+            minCost[i][0] = minCost[i - 1][0] + cost[i][0];
+        }
+        //The first row case, each cell can only be computed from previous one.
+        for (int j = 1; j < cost[0].length; j++) {
+            minCost[0][j] = minCost[0][j - 1] + cost[0][j];
+        }
+        for (int i = 1; i < cost.length; i++) {
+            for (int j = 1; j < cost[0].length; j++) {
+                //minCost(m, n) = min (minCost(m-1, n-1), minCost(m-1, n), minCost(m, n-1)) + cost[m][n]
+                minCost[i][j] = min(minCost[i - 1][j - 1], minCost[i - 1][j], minCost[i][j - 1]) + cost[i][j];
+            }
+        }
+        
+        return minCost[cost.length - 1][cost[0].length - 1];
+    }
+    
+    public static void main(String[] args) {
+        int cost[][] = {{1, 2, 3},
+                        {4, 8, 2},
+                        {1, 5, 3}};
+        MinimumPathSum mp = new MinimumPathSum();
+        System.out.println(mp.minCostPathNaive(cost, 2, 2) + ", " + mp.minCostPath(cost, 2, 2));
+        
+    }
 }
