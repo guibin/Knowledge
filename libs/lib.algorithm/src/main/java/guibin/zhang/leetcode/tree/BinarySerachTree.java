873dc5fefcfd65d0b4766484a3b81ef27e96ad40
Guibin Zhang <gzhang@radiumone.com>
2014-08-26 09:31:33 -0700

    Clean up, merge ValidateBinarySearchTree.java to BinarySerachTree.java

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/tree/BinarySerachTree.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/tree/BinarySerachTree.java
@@ -43,6 +43,49 @@ public class BinarySerachTree {
         return isBST(node, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
     
+    
+    /**
+     * Given a binary tree, determine if it is a valid binary search tree (BST).
+     *
+     * Assume a BST is defined as follows:
+     *
+     * The left subtree of a node contains only nodes with keys less than the
+     * node's key. The right subtree of a node contains only nodes with keys
+     * greater than the node's key. Both the left and right subtrees must also
+     * be binary search trees.
+     *
+     * http://www.ardendertat.com/2011/10/10/programming-interview-questions-7-binary-search-tree-check/
+     *
+     * 递归地判断当前节点是否大于**左**孩子，并且是否大于其左孩子的每一个**右**子孙；
+     * 同理，判断当前节点是否小于**右**孩子，并且是否小于其右孩子的每一个**左**子孙
+     *
+     * @param root
+     * @return
+     */
+    public boolean isBST_v2(TreeNode root) {
+
+//        return isValidBST(root, new TreeNode(Integer.MIN_VALUE));
+        return isBST_v2(root, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+
+    public boolean isBST_v2(TreeNode root, int max, int min) {
+
+        if (root == null) {
+            return true;
+        }
+
+        if (!isBST_v2(root.left, root.val, min)) {
+            return false;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        if (!isBST_v2(root.right, max, root.val)) {
+            return false;
+        }
+        return true;
+    }
+    
     /**
      * This is an invalid BST 
      *    10
