7cc15a76c03d8904085bbec2fb5849e1cbb30fd7
Guibin Zhang <gzhang@radiumone.com>
2014-09-06 10:15:08 -0700

    Add Max Priority Queue

---
+++ libs/lib.algorithm/src/main/java/guibin/zhang/datastructure/tree/MaxPriorityQueue.java
@@ -0,0 +1,96 @@
+package guibin.zhang.datastructure.tree;
+
+/**
+ *
+ * https://d396qusza40orc.cloudfront.net/algs4partI/slides/24PriorityQueues.pdf
+ * 
+ * Binary heap: Array representation of a heap-ordered complete binary tree.
+ * Basically binary heap is a complete binary tree.
+ * 
+ * Binary heap has features:
+ * 1. Parent's key >= children's keys.
+ * 
+ * Array representation of the MaxHeap
+ * 1. index starts from 1
+ * 2. Take nodes in **level** order.
+ * 3. No explicit link needed.
+ * 
+ * Attribute:
+ * 1. Largest key is a[1], which is the root of the binary tree.
+ * 2. Parent of node K is at K/2.
+ * 3. Children of node K are at 2K and 2K + 1.
+ * 
+ * How to eliminate the violation:
+ * 1. Exchange key in child with key in parent.
+ * 2. Repeat until heap order restored.
+ * 
+ * @author Guibin Zhang <guibin.beijing@gmail.com>
+ */
+public class MaxPriorityQueue<T extends Comparable<T>> {
+    
+    private T[] pq;
+    private int N;
+    
+    public MaxPriorityQueue(int capacity) {
+        pq = (T[]) new Comparable[capacity + 1];
+    }
+    
+    /**
+     * Eliminate the violation from children to parent, from bottom up.
+     * @param K 
+     */
+    private void swim(int K) {
+        //When parent less than child, exchange root with child. K/2 is parent, K is child
+        while (K > 1 && pq[K/2].compareTo(pq[K]) < 0) {
+            exch(K/2, K);
+            K = K/2;
+        }
+    }
+    
+    /**
+     * Eliminate violation from parent to children, from top down.
+     * 
+     * @param K 
+     */
+    private void sink(int K) {
+       while (K * 2 < N) {
+           int j = 2*K;
+           //Find out the bigger child
+           if (j < N && pq[j].compareTo(pq[j + 1]) < 0) j++;
+           //If root < children
+           if (pq[K].compareTo(pq[j]) < 0) {
+               //Exchange root with the bigger child
+               exch(K, j);
+               //Keep on sink down
+               K = j;
+           } else {
+               break;
+           }
+       } 
+    }
+    
+    public void insert(T x) {
+        pq[++N] = x;
+        swim(N);
+    }
+    
+    public T delMax() {
+        T max = pq[1];
+        //Move the last element to the root
+        exch(1, N --);
+        sink(1);
+        //Prevent memroy leak
+        pq[N + 1] = null;
+        return max;
+    }
+    
+    public boolean isEmpty() {
+        return N == 0;
+    }
+    
+    private void exch(int i, int j) {
+        T tmp = pq[i];
+        pq[i] = pq[j];
+        pq[j] = tmp;
+    }
+}
