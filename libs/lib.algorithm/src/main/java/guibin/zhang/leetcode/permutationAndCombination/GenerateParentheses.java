7467447ea7bc8c3b29b104dc935c01255d23d6eb
Guibin Zhang <gzhang@radiumone.com>
2014-09-14 15:54:00 -0700

    Clean up

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/permutationAndCombination/GenerateParentheses.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/permutationAndCombination/GenerateParentheses.java
@@ -17,36 +17,36 @@ import java.util.List;
  */
 public class GenerateParentheses {
     
-    public ArrayList<String> generateParenthesis(int n) {
+    public List<String> generateParenthesis(int n) {
         
-        ArrayList<String> result = new ArrayList<String>();
+        List<String> result = new ArrayList<>();
         char[] array = new char[n * 2];
         generate(n, 0, 0, array, result);
         return result;
     }
     
-    public void generate(int n, int left, int right, char[] array, ArrayList<String> result) {
+    public void generate(int n, int left, int right, char[] branch, List<String> result) {
         
         int k = left + right;
         if (k == 2 * n) {
-            result.add(new String(array));
+            result.add(new String(branch));
             return;
         }
         if (left < n) {
-            array[k] = '(';
-            generate(n, left + 1, right, array, result);
+            branch[k] = '(';
+            generate(n, left + 1, right, branch, result);
         }
         if (right < left) {
-            array[k] = ')';
-            generate(n, left, right + 1, array, result);
+            branch[k] = ')';
+            generate(n, left, right + 1, branch, result);
         }
     }
     
     public static void main(String[] args) {
         GenerateParentheses gp = new GenerateParentheses();
         List<String> result = gp.generateParenthesis(3);
-        for (String s : result) {
+        result.stream().forEach((s) -> {
             System.out.println(s);
-        }
+        });
     }
 }
