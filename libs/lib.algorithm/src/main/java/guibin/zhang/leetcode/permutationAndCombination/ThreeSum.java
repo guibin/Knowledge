543c4816ff1ee1c7a61630483b213a30e4504286
Guibin Zhang <gzhang@radiumone.com>
2014-09-13 22:47:10 -0700

    Add some comments to 3Sum

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/permutationAndCombination/ThreeSum.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/permutationAndCombination/ThreeSum.java
@@ -30,18 +30,23 @@ public class ThreeSum {
         Arrays.sort(num);
         
         for (int i = 0; i < len - 2; i ++) {
+            //num[i] num[i - 1]
             if (i > 0 && num[i] == num[i - 1]) continue;
             int j = i + 1;
             int k = len - 1;
+            
             while (j < k) {
-                if (j > i + 1 && num[j] == num[j - 1]) {
+                //num[j] num[j - 1]
+                if (j - 1 > i && num[j] == num[j - 1]) {
                     j ++;
                     continue;
                 }
-                if (k < len - 1 && num[k] == num[k + 1]) {
+                //num[k] num[k + 1]
+                if (k + 1< len && num[k] == num[k + 1]) {
                     k --;
                     continue;
                 }
+                
                 int target = num[i] + num[j] + num[k];
                 if (target == 0) {
                     ArrayList<Integer> row = new ArrayList<Integer>();
