063823826c3f89b98701301f3629d993277c5d06
Guibin Zhang <gzhang@radiumone.com>
2014-09-14 11:04:25 -0700

    Add comments to ThreeSumClosest

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/permutationAndCombination/ThreeSumClosest.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/permutationAndCombination/ThreeSumClosest.java
@@ -3,12 +3,18 @@ package guibin.zhang.leetcode.permutationAndCombination;
 import java.util.Arrays;
 
 /**
- *
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. 
+ * Return the sum of the three integers. 
+ * You may assume that each input would have exactly one solution.
+ * 
+ *   For example, given array S = {-1 2 1 -4}, and target = 1.
+ * 
+ *   The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
  * 
  * 
  * @author Gubin Zhang <guibin.beijing@gmail.com>
  */
-public class ThreeSumCloset {
+public class ThreeSumClosest {
     public int threeSumClosest(int[] num, int target) {
         // Start typing your Java solution below
         // DO NOT write main() function
@@ -43,7 +49,7 @@ public class ThreeSumCloset {
     }
     
     public static void main(String[] args) {
-        ThreeSumCloset ts = new ThreeSumCloset();
+        ThreeSumClosest ts = new ThreeSumClosest();
         int[] num = {0, 0, 0};
         System.out.println(ts.threeSumClosest(num, 1));
     }
