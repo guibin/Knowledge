d8ce89c314558902cd62a6dd6ddbe516dec01735
Guibin Zhang <gzhang@radiumone.com>
2014-09-17 12:20:13 -0700

    Refactor PartitionList, use dummy head to make logic simpler

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/PartitionList.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/PartitionList.java
@@ -31,106 +31,33 @@ public class PartitionList {
     }
 
     public ListNode partition(ListNode head, int x) {
-        
-        ListNode sHead = null;
-        ListNode sCurr = null;
-        ListNode bHead = null;
         ListNode curr = head;
-        ListNode last = null;
-        
-        while(curr != null) {
-            if(sHead != null && bHead != null) {
-                if(curr.val < x) {
-                    sCurr.next = curr;
-                    sCurr = curr;
-                    curr = curr.next;
-                    sCurr.next = null;//Note: when connect a node, be sure to break the node after that.
-                } else {
-                    last.next = curr;
-                    last = curr;
-                    curr = curr.next;
-                    last.next = null;
-                }
-            } else if(sHead == null && bHead != null) {
-                if(curr.val < x) {
-                    sHead = curr;
-                    sCurr = curr;
-                    curr = curr.next;
-                    sCurr.next = null;
-                } else {
-                    last.next = curr;
-                    last = curr;
-                    curr = curr.next;
-                    last.next = null;
-                }
-            } else if(sHead != null && bHead == null) {
-                if(curr.val < x) {
-                    sCurr.next = curr;
-                    sCurr = curr;
-                    curr = curr.next;
-                    sCurr.next = null;
-                } else {
-                    bHead = curr;
-                    last = curr;
-                    curr = curr.next;
-                    last.next = null;
-                }
-            } else { //both are null
-                if(curr.val < x) {
-                    sHead = curr;
-                    sCurr = curr;
-                    curr = curr.next;
-                    sCurr.next = null;
-                } else {
-                    bHead = curr;
-                    last = curr;
-                    curr = curr.next;
-                    last.next = null;
-                }
-            }
-        }
-        
-        if(sHead != null) {
-            sCurr.next = bHead;
-            return sHead;
-        } else {
-            return head;
-        }
-    }
-    
-    public ListNode partition_v2(ListNode head, int x) {
-        ListNode curr = head;
-        ListNode sHead = null;
-        ListNode sCurr = null;
-        ListNode bHead = null;
-        ListNode bCurr = null;
+        ListNode sHead = new ListNode(0);
+        ListNode sCurr = sHead;
+        ListNode bHead = new ListNode(0);
+        ListNode bCurr = bHead;
         
         while (curr != null) {
             if (curr.val < x) {
-                if (sHead == null) {
-                    sHead = curr;
-                } else {
-                    sCurr.next = curr;
-                }
-                sCurr = curr;
+                sCurr.next = curr;
+                
+                sCurr = sCurr.next;
                 curr = curr.next;
                 sCurr.next = null;
             } else {
-                if(bHead == null) {
-                    bHead = curr;
-                } else {
-                    bCurr.next = curr;
-                }
-                bCurr = curr;
+                bCurr.next = curr;
+                
+                bCurr = bCurr.next;
                 curr = curr.next;
                 bCurr.next = null;
             }
         }
-        if(sHead != null) {
-            sCurr.next = bHead;
-            return sHead;
+        
+        if(sHead != sCurr) {
+            sCurr.next = bHead.next;
+            return sHead.next;
         } else {
-            return bHead;
+            return bHead.next;
         }
     }
     
@@ -140,7 +67,7 @@ public class PartitionList {
         ListNode b = pl.new ListNode(1);
         a.next = b;
         
-        ListNode curr = pl.partition_v2(a, 2);
+        ListNode curr = pl.partition(a, 2);
         while(curr != null) {
             System.out.print(curr.val + ",");
             curr = curr.next;
