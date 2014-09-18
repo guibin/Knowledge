a1da8a01d3c330c65cbce64fad8671b8b7f96273
Guibin Zhang <gzhang@radiumone.com>
2014-09-18 15:45:30 -0700

    Move shuffle to a separated file

--- libs/lib.algorithm/src/main/java/guibin/zhang/onlinecourse/QuickSort.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/onlinecourse/QuickSort.java
@@ -83,22 +83,6 @@ public class QuickSort {
         sort(a, j + 1, hi);
     }
     
-    /**
-     * Knuth shuffle.
-     * In iteration i, pick integer r between i and N-1 uniformly at random,
-     * Or between 0 to i uniformly at random
-     * Swap a[i] and a[r]
-     * @param a 
-     */
-    public void shuffle(Object[] a) {
-        int N = a.length;
-        for (int i = 0; i < N; i++) {
-            //uniformly random number between i and n-1 inclusive
-            int r = i + (int)(Math.random() * (N - i));
-            swap(a, i, r);
-        }
-    }
-    
     private void swap(Object[] a, int i, int j) {
         Object tmp = a[i];
         a[i] = a[j];
@@ -128,9 +112,5 @@ public class QuickSort {
         Arrays.stream(a).forEach((c) -> {System.out.print(c + ",");});
         System.out.println();
         
-        System.out.println("After shffule");
-        s.shuffle(a);
-        Arrays.stream(a).forEach((c) -> System.out.print(c + ","));
-        System.out.println();
     }
 }
---
+++ libs/lib.algorithm/src/main/java/guibin/zhang/onlinecourse/Shuffle.java
@@ -0,0 +1,39 @@
+package guibin.zhang.onlinecourse;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Guibin Zhang <guibin.beijing@gmail.com>
+ */
+public class Shuffle {
+    /**
+     * Knuth shuffle.
+     * In iteration i, pick integer r between i and N-1 uniformly at random,
+     * Or between 0 to i uniformly at random
+     * Swap a[i] and a[r]
+     * @param a 
+     */
+    public void shuffle(Object[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            //uniformly random number between i and n-1 inclusive
+            int r = i + (int)(Math.random() * (N - i));
+            swap(a, i, r);
+        }
+    }
+    
+    private void swap(Object[] a, int i, int j) {
+        Object tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    
+    public static void main(String[] args) {
+        Comparable[] a = {50, 61, 44, 32, 99, 87, 51, 50, 50, 12};
+        Shuffle sf = new Shuffle();
+        sf.shuffle(a);
+        Arrays.stream(a).forEach((c) -> System.out.print(c + ","));
+        System.out.println();
+    }
+}
