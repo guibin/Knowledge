189b812f557cc3b4a10b2ac871ab2c27bdade42c
Guibin Zhang <gzhang@radiumone.com>
2014-09-20 07:03:47 -0700

    Clean up

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/tree/MaximumDepthOfBinaryTree.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/tree/MaximumDepthOfBinaryTree.java
@@ -26,10 +26,8 @@ public class MaximumDepthOfBinaryTree {
     
     public int maxDepth(TreeNode root) {
         
-        if(root == null) {
-            return 0;
-        }
-        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        if(root == null) return 0;
         
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
