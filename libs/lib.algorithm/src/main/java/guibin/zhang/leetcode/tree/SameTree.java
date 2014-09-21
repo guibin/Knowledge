cb4610687968063c2e481eb6238c8371139b8920
Guibin Zhang <gzhang@radiumone.com>
2014-09-20 20:22:28 -0700

    Refactor same tree

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/tree/SameTree.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/tree/SameTree.java
@@ -31,18 +31,11 @@ public class SameTree {
      */
     public boolean isSameTree(TreeNode p, TreeNode q) {
         
-        if(p == null && q == null) {
-            return true;
-        } else if(p != null && q == null) {
-            return false;
-        } else if(p == null && q != null) {
-            return false;
-        } else {
-            if(p.val != q.val) {
-                return false;
-            } else {
-                return isSameTree(p.left,q.left) && isSameTree(p.right, q.right);
-            }
-        }
+        if (p == null && q == null) return true;
+        if (p != null && q == null) return false;
+        if (p == null && q != null) return false;
+        if (p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        
     }
 }
