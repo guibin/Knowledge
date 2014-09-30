6d95b9d29476ac30628df47603a387dda792a571
Guibin Zhang <gzhang@radiumone.com>
2014-09-29 21:25:37 -0700

    Add new method to Search In Rotated Sorted Array

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/SearchInRotatedSortedArray.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/SearchInRotatedSortedArray.java
@@ -18,6 +18,34 @@ package guibin.zhang.leetcode.listAndArray;
  */
 public class SearchInRotatedSortedArray {
 
+    public int search_v2 (int[] A, int target) {
+        int start = 0;
+        int end = A.length - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return mid;
+            }
+            
+            if (A[mid] >= A[start]) {//Note: Here is >=
+                if (A[mid] > target && A[start] <= target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (A[mid] < target && A[end] >= target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } 
+        }
+        return -1;
+    }
+    
+    
     public int search(int[] A, int target) {
         int start = 0;
         int end = A.length - 1;
@@ -26,24 +54,31 @@ public class SearchInRotatedSortedArray {
         while (start <= end) {
             if (A[mid] == target) {
                 return mid;
-            } else {
-                if (A[mid] < A[end]) {
-                    if (A[mid] < target && target <= A[end]) {
-                        start = mid + 1;
-                    } else {
-                        end = mid - 1;
-                    }
-                    mid = start + (end - start) / 2;
+            }
+            
+            if (A[mid] < A[end]) {
+                if (A[mid] < target && target <= A[end]) {
+                    start = mid + 1;
                 } else {
-                    if (A[mid] > target && target >= A[start]) {
-                        end = mid - 1;
-                    } else {
-                        start = mid + 1;
-                    }
-                    mid = start + (end - start) / 2;
+                    end = mid - 1;
                 }
+                mid = start + (end - start) / 2;
+            } else {
+                if (A[mid] > target && target >= A[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+                mid = start + (end - start) / 2;
             }
         }
         return -1;
+    }
+    
+    public static void main(String[] args) {
+        SearchInRotatedSortedArray si = new SearchInRotatedSortedArray();
+        int[] A = {3,1};
+        
+        System.out.println(si.search_v2(A, 1));
     }
 }
--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/SearchInRotatedSortedArrayII.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/SearchInRotatedSortedArrayII.java
@@ -16,6 +16,41 @@ package guibin.zhang.leetcode.listAndArray;
  * @author Gubin Zhang <guibin.beijing@gmail.com>
  */
 public class SearchInRotatedSortedArrayII {
+    
+    public boolean search_v2(int[] A, int target) {
+        
+        if (A == null || A.length == 0) {
+            return false;
+        }
+        int start = 0;
+        int end = A.length - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return true;
+            }
+            
+            if (A[mid] > A[start]) {
+                if (A[mid] > target && A[start] <= target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (A[mid] < A[start]) {
+                if (A[mid] < target && A[end] >= target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                start++;
+            }
+        }
+        return false;
+    }
+    
+    
     public boolean search(int[] A, int target) {
         
         int start = 0;
@@ -78,6 +113,6 @@ public class SearchInRotatedSortedArrayII {
         
         SearchInRotatedSortedArrayII si = new SearchInRotatedSortedArrayII();
         int[] A = {3,1,2,2,2};
-        System.out.println(si.search(A, 1));
+        System.out.println(si.search_v2(A, 1));
     }
 }
