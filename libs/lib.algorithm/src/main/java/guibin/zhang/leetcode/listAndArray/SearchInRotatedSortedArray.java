4c8eee404fc60a962105475617f9a888a23ff824
Guibin Zhang <gzhang@radiumone.com>
2014-09-30 20:52:09 -0700

    Add a new question separate point of the rotated array

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/SearchInRotatedSortedArray.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/SearchInRotatedSortedArray.java
@@ -45,6 +45,34 @@ public class SearchInRotatedSortedArray {
         return -1;
     }
     
+    /**
+     * Search the separate point of the rotated array.
+     * EG. {4,5,6,7,2,3,4} => return 4
+     * 
+     * @param A
+     * @return 
+     */
+    public int search(int[] A) {
+        
+        //Use "left" to judge which part mid is pointing to
+        int start = 0, left = 0;
+        int end = A.length - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            //mid is in right part
+            if (A[mid] < A[left]) {
+                if (A[mid] < A[mid - 1]) {
+                    return mid;
+                } else {
+                    end = mid - 1;
+                }
+            } else {//mid is in the left part
+                start = mid + 1;
+            }
+        }
+        return 0;
+    }
     
     public int search(int[] A, int target) {
         int start = 0;
@@ -77,8 +105,11 @@ public class SearchInRotatedSortedArray {
     
     public static void main(String[] args) {
         SearchInRotatedSortedArray si = new SearchInRotatedSortedArray();
-        int[] A = {3,1};
+        int[] A = {4,5,6,7,2,3,4};//4
+        int[] B = {4,5,6,7,2};//4
+        int[] C = {4,5,6,7};//0
+        int[] E = {9,4,5,6,7};//1
         
-        System.out.println(si.search_v2(A, 1));
+        System.out.println(si.search(E));
     }
 }
