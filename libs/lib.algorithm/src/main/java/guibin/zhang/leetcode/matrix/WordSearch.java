62e06793c2a3efd729d97b577417b0bfa7ebe137
Guibin Zhang <gzhang@radiumone.com>
2014-10-03 16:21:10 -0700

    Refactor word search in matrix

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/matrix/WordSearch.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/matrix/WordSearch.java
@@ -23,57 +23,6 @@ package guibin.zhang.leetcode.matrix;
  */
 public class WordSearch {
     
-    public boolean exist_tte(char[][] board, String word) {
-        
-        int rows = board.length;
-        int cols = board[0].length;
-        
-        if(rows == 1 && cols == 1 && word.length() == 1 && board[0][0] == word.charAt(0)) {
-            return true;
-        }
-        if(rows * cols < word.length()) {
-            return false;
-        }
-        
-        //This flag is used to avoid the duplicated visit.
-        boolean[][] visited = null;
-        
-        for (int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                visited = new boolean[rows][cols];
-                if(search_tte(board, i, j, word, 0, visited)) {
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-    
-    public boolean search_tte(char[][] board, int i, int j, String word, int idx, boolean[][] visited) {
-        
-        int rows = board.length;
-        int cols = board[0].length;
-
-        if (i < 0 || i >= rows || j < 0 || j >= cols || idx > word.length() - 1 || visited[i][j]) {
-            return false;
-        } 
-
-        if (board[i][j] == word.charAt(idx)) {
-            visited[i][j] = true;
-            if (idx == word.length() - 1) {
-                return true;
-            } else {
-                return search_tte(board, i - 1, j, word, idx + 1, visited)
-                        || search_tte(board, i + 1, j, word, idx + 1, visited)
-                        || search_tte(board, i, j - 1, word, idx + 1, visited)
-                        || search_tte(board, i, j + 1, word, idx + 1, visited);
-            }
-        } else {
-            return false;
-        }
-    }
-    
     /**
      * Run Status: Accepted!
      * Program Runtime: 712 milli secs
@@ -84,22 +33,19 @@ public class WordSearch {
      * @return 
      */
     public boolean exist(char[][] board, String word) {
+        
         int rows = board.length;
         int cols = board[0].length;
         
-        if(rows == 1 && cols == 1 && word.length() == 1 && board[0][0] == word.charAt(0)) {
-            return true;
-        }
         if(rows * cols < word.length()) {
             return false;
         }
         
-        //This flag is used to avoid the duplicated visit.
         boolean[][] visited = new boolean[rows][cols];
         
         for (int i = 0; i < rows; i++) {
             for(int j = 0; j < cols; j++) {
-                if(search(board, i, j, word, 0, visited)) {
+                if (search(board, word, i, j, 0, visited)) {
                     return true;
                 }
             }
@@ -108,43 +54,28 @@ public class WordSearch {
         return false;
     }
     
-    public boolean search(char[][] board, int i, int j, String word, int idx, boolean[][] visited) {
+    public boolean search(char[][] board, String word, int i, int j,  int idx, boolean[][] visited) {
         
         int rows = board.length;
         int cols = board[0].length;
 
-        if (board[i][j] != word.charAt(idx)) {
-           return false; 
-        }
-        else {
-            visited[i][j] = true;
-            if (idx == word.length() - 1) {
-                return true;
-            } else {
-                
-                if(i - 1 >= 0 && idx + 1 < word.length() && !visited[i - 1][j] && 
-                        search(board, i - 1, j, word, idx + 1, visited)) {
-                    return true;
-                }
-                
-                if(i + 1 < rows && idx + 1 < word.length() && !visited[i + 1][j] && 
-                        search(board, i + 1, j, word, idx + 1, visited)) {
-                    return true;
-                }
-                
-                if(j - 1 >= 0 && idx + 1 < word.length() && !visited[i][j - 1] && 
-                        search(board, i, j - 1, word, idx + 1, visited)) {
-                    return true;
-                }
-                
-                if(j + 1 < cols && idx + 1 < word.length() && !visited[i][j + 1] && 
-                        search(board, i, j + 1, word, idx + 1, visited)) {
-                    return true;
-                }
-                visited[i][j] = false;
-                return false;
-            }
-        }
+        //Go to the end of the target
+        if (idx == word.length()) return true;
+        
+        //Go to outside of board
+        if (i < 0 || i >= rows || j < 0 || j >= cols) return false;
+        
+        //Has been visited or not the target
+        if (visited[i][j] || board[i][j] != word.charAt(idx)) return false;
+        
+        visited[i][j] = true;
+        boolean result = search(board, word, i - 1, j, idx + 1, visited) || 
+                search(board, word, i + 1, j, idx + 1, visited) || 
+                search(board, word, i, j - 1, idx + 1, visited) || 
+                search(board, word, i, j + 1, idx + 1, visited);
+        visited[i][j] = false;
+        
+        return result;
     }
 }
 
