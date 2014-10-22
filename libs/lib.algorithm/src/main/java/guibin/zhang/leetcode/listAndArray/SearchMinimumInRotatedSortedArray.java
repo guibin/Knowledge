b348cbf9798b2985e0882bfd9f060c18b48e8688
Guibin Zhang <gzhang@radiumone.com>
2014-10-21 22:41:55 -0700

    Find Minimum in Rotated Sorted Array

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/SearchMinimumInRotatedSortedArray.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/listAndArray/SearchMinimumInRotatedSortedArray.java
@@ -2,6 +2,8 @@ package guibin.zhang.leetcode.listAndArray;
 
 /**
  * 
+ * Find Minimum in Rotated Sorted Array.
+ * 
  * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
  * 
  * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
