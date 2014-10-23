378c0d6aab3ff9223a7e4537971777fd73b0ad28
Guibin Zhang <gzhang@radiumone.com>
2014-10-23 06:17:29 -0700

    Add Str of Str

---
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/string/StrStr.java
@@ -0,0 +1,30 @@
+package guibin.zhang.leetcode.string;
+
+/**
+ *
+ * Implement strStr().
+ * 
+ * Returns a pointer to the first occurrence of needle in haystack, or null if needle is not part of haystack.
+ * 
+ * @author Guibin Zhang <guibin.beijing@gmail.com>
+ */
+public class StrStr {
+    
+    public String strStr(String haystack, String needle) {
+        
+        if(needle.length() == 0) return haystack;
+          
+         int n = 0;
+          
+         for(int start = 0; start <= haystack.length() - needle.length(); start++){
+             n = 0;
+              
+             while(n < needle.length() && haystack.charAt(start + n) == needle.charAt(n))
+                  n++;
+                   
+             if(n == needle.length()) return haystack.substring(start);
+         }
+          
+         return null;
+    }
+}
