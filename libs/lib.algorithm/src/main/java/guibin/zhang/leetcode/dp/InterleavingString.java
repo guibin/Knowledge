93f15c30800bec1f62f8504ab700a35bd7f49d2c
Guibin Zhang <gzhang@radiumone.com>
2014-10-25 07:05:31 -0700

    Clean up InterleavingString

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/dp/InterleavingString.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/dp/InterleavingString.java
@@ -12,6 +12,48 @@ package guibin.zhang.leetcode.dp;
  */
 public class InterleavingString {
 
+    /**
+     * The explanation of the algorithm, and corresponding proof:
+     * http://blog.csdn.net/u011095253/article/details/9248073
+     * 
+     * http://blog.theliuy.com/interleaving-string/
+     * http://yucoding.blogspot.com/2013/01/leetcode-question-27-interleaving-string.html
+     * 
+     * This a 2D DP(Dynamic Programming) problem. 
+     * 
+     * Use match[i][j] to save if s1[i] and s2[j] are matched s3[i+j]. 
+     * 
+     * match[i][j] =   (s3[i+j]==s1[i]  && match[i-1][j])
+     *                     || (s3[i+j] ==s2[j] && match[i][j-1])
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return 
+     */
+    public boolean isInterleave_v2(String s1, String s2, String s3) {
+
+        if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        boolean[][] mat = new boolean[s1.length() + 1][s2.length() + 1];
+        mat[0][0] = true;
+        for (int i = 1; i <= s1.length(); ++i) {
+            mat[i][0] = mat[i - 1][0] && (s3.charAt(i - 1) == s1.charAt(i - 1));
+        }
+        for (int i = 1; i <= s2.length(); ++i) {
+            mat[0][i] = mat[0][i - 1] && (s3.charAt(i - 1) == s2.charAt(i - 1));
+        }
+        for (int i = 1; i <= s1.length(); ++i) {
+            for (int j = 1; j <= s2.length(); ++j) {
+                mat[i][j] = (mat[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1)) || 
+                        (mat[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
+            }
+        }
+        return mat[s1.length()][s2.length()];
+    }
+    
+    
+    
     public boolean isInterleave(String s1, String s2, String s3) {
 
         return isInterleave(s1, 0, s2, 0, s3, 0);
@@ -53,47 +95,5 @@ public class InterleavingString {
             }
         }
     }
-
     
-    /**
-     * The explanation of the algorithm, and corresponding proof:
-     * http://lifestruthonwisdom.blogspot.com/2012/09/dynamic-programming-practice-problems_28.html .
-     * 
-     * http://blog.theliuy.com/interleaving-string/
-     * http://yucoding.blogspot.com/2013/01/leetcode-question-27-interleaving-string.html
-     * 
-     * This a 2D DP(Dynamic Programming) problem. 
-     * 
-     * Use match[i][j] to save if s1[i] and s2[j] are matched s3[i+j]. 
-     * 
-     * match[i][j] =   (s3[i+j]==s1[i]  && match[i-1][j])
-     *                     || (s3[i+j] ==s2[j] && match[i][j-1])
-     * @param s1
-     * @param p1
-     * @param s2
-     * @param p2
-     * @param s3
-     * @param p3
-     * @return 
-     */
-    public boolean isInterleave_v2(String s1, int p1, String s2, int p2, String s3, int p3) {
-
-        if (s1.length() + s2.length() != s3.length()) {
-            return false;
-        }
-        boolean[][] mat = new boolean[s1.length() + 1][s2.length() + 1];
-        mat[0][0] = true;
-        for (int i = 1; i <= s1.length(); ++i) {
-            mat[i][0] = mat[i - 1][0] && (s3.charAt(i - 1) == s1.charAt(i - 1));
-        }
-        for (int i = 1; i <= s2.length(); ++i) {
-            mat[0][i] = mat[0][i - 1] && (s3.charAt(i - 1) == s2.charAt(i - 1));
-        }
-        for (int i = 1; i <= s1.length(); ++i) {
-            for (int j = 1; j <= s2.length(); ++j) {
-                mat[i][j] = (mat[i][j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1)) || (mat[i - 1][j] && s1.charAt(i - 1) == s3.charAt(i + j - 1));
-            }
-        }
-        return mat[s1.length()][s2.length()];
-    }
 }
