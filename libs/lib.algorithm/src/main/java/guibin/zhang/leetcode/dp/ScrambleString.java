ede281aaa4d403ceb4ae9753084a6d30213c937e
Guibin Zhang <gzhang@radiumone.com>
2014-10-25 11:41:42 -0700

    Move ScrambleString to package dp

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/dp/ScrambleString.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/dp/ScrambleString.java
@@ -1,4 +1,4 @@
-package guibin.zhang.datastructure.tree;
+package guibin.zhang.leetcode.dp;
 
 /**
  * 
@@ -50,6 +50,55 @@ package guibin.zhang.datastructure.tree;
 public class ScrambleString {
     
     /**
+     * http://blog.sina.com.cn/s/blog_b9285de20101gy6n.html
+     * 
+     * dp[i][j][k] 代表了s1从i开始，s2从j开始，长度为k的两个substring是否为scramble string.
+     * 
+     * 有三种情况需要考虑：
+     * 
+     * 1. 如果两个substring相等的话，则为true
+     * 
+     * 2. 如果两个substring中间某一个点，左边的substrings为scramble string， 
+     *    同时右边的substrings也为scramble string，则为true
+     * 
+     * 3. 如果两个substring中间某一个点，s1左边的substring和s2右边的substring为scramble string, 
+     *    同时s1右边substring和s2左边的substring也为scramble string，则为true
+     * 
+     * @param s1
+     * @param s2
+     * @return 
+     */
+    public boolean isScramble_v2(String s1, String s2) {
+
+        int n = s1.length();
+        boolean[][][] dp = new boolean[n][n][n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                for (int k = 1; k + Math.max(i, j) <= n; k++) {
+                    if (s1.substring(i, i + k).equals(s2.substring(j, j + k))) {
+                        dp[i][j][k] = true;
+                    } else {
+                        for (int m = 1; m < k; m++) {
+                            
+                            if (dp[i][j][m] && dp[i + m][j + m][k - m] || 
+                                    dp[i][j + k - m][m] && dp[i + m][j][k - m]) {
+                                
+                                dp[i][j][k] = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return dp[0][0][n];
+    }
+    
+    
+    
+    /**
      * http://discuss.leetcode.com/questions/262/scramble-string
      * 
      * Given a string A and a string B, if B is a scramble string of A, 
@@ -70,8 +119,8 @@ public class ScrambleString {
      * The following code try to divide B at each position (from 1 to B.length-1), 
      * and if we found the valid division,check whether the two parts are scramble string of A1, A2 recursively.
      * 
-     * @param s1
-     * @param s2
+     * @param T
+     * @param C
      * @return 
      */
     public boolean isScramble(String T, String C) {
@@ -120,50 +169,4 @@ public class ScrambleString {
         return true;
     }
     
-    /**
-     * http://blog.sina.com.cn/s/blog_b9285de20101gy6n.html
-     * 
-     * dp[i][j][k] 代表了s1从i开始，s2从j开始，长度为k的两个substring是否为scramble string.
-     * 
-     * 有三种情况需要考虑：
-     * 
-     * 1. 如果两个substring相等的话，则为true
-     * 
-     * 2. 如果两个substring中间某一个点，左边的substrings为scramble string， 
-     *    同时右边的substrings也为scramble string，则为true
-     * 
-     * 3. 如果两个substring中间某一个点，s1左边的substring和s2右边的substring为scramble string, 
-     *    同时s1右边substring和s2左边的substring也为scramble string，则为true
-     * 
-     * @param s1
-     * @param s2
-     * @return 
-     */
-    public boolean isScramble_v2(String s1, String s2) {
-
-        int n = s1.length();
-        boolean[][][] dp = new boolean[n][n][n + 1];
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                for (int k = 1; k <= n - Math.max(i, j); k++) {
-                    if (s1.substring(i, i + k).equals(s2.substring(j, j + k))) {
-                        dp[i][j][k] = true;
-                    } else {
-                        for (int m = 1; m < k; m++) {
-                            
-                            if (dp[i][j][m] && dp[i + m][j + m][k - m] || 
-                                    dp[i][j + k - m][m] && dp[i + m][j][k - m]) {
-                                
-                                dp[i][j][k] = true;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return dp[0][0][n];
-    }
 }
