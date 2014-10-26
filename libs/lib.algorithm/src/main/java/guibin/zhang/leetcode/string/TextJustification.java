6bd575383e35c866334fe7f48402bcfda60db307
Guibin Zhang <gzhang@radiumone.com>
2014-10-25 22:30:33 -0700

    Add new method to Text Justification

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/string/TextJustification.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/string/TextJustification.java
@@ -1,6 +1,7 @@
-package guibin.zhang.leetcode;
+package guibin.zhang.leetcode.string;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Given an array of words and a length L, format the text such that each line 
@@ -39,7 +40,58 @@ import java.util.ArrayList;
  * @author Gubin Zhang <guibin.beijing@gmail.com>
  */
 public class TextJustification {
-
+    
+    /**
+     * http://www.elvisyu.com/text-justification/#more-315
+     * @param words
+     * @param L
+     * @return 
+     */
+    public List<String> fullJustify_v2(String[] words, int L) {
+        
+        int wordsCount = words.length;
+        List<String> result = new ArrayList<>();
+        
+        int curLen = 0;
+        int lastI = 0; //lastI represent the sequence of each word in every row
+        for (int i = 0; i <= wordsCount; i++) {
+            //only process when row length larger than L or at the end
+            if (i == wordsCount || curLen + words[i].length() + i - lastI > L) {
+                StringBuffer buf = new StringBuffer();
+                int spaceCount = L - curLen;
+                int spaceSlots = i - lastI - 1;
+                if (spaceSlots == 0 || i == wordsCount) {    //add space case 1
+                    for(int j = lastI; j < i; j++){
+                        buf.append(words[j]);
+                        if(j != i - 1)
+                            appendSpace(buf, 1);
+                    }
+                    appendSpace(buf, L - buf.length());
+                } else {                                     //add space case 2
+                    int spaceEach = spaceCount / spaceSlots;
+                    int spaceExtra = spaceCount % spaceSlots;
+                    for (int j = lastI; j < i; j++) {    
+                        buf.append(words[j]);
+                        if (j != i - 1)
+                            appendSpace(buf, spaceEach + (j - lastI < spaceExtra ? 1 : 0));
+                    }
+                }
+                result.add(buf.toString());
+                lastI = i;
+                curLen = 0;
+            }
+            if (i < wordsCount)               //keep finding words if it doesn't end or row length is smaller than L
+                curLen += words[i].length();
+        }
+        return result;
+    }
+    
+    private void appendSpace(StringBuffer sb, int count) {
+        for (int i = 0; i < count; i++)
+            sb.append(' ');
+    }
+    
+    
     public ArrayList<String> fullJustify(String[] words, int L) {
 
         ArrayList<String> result = new ArrayList<String>();
