c9c9b3cace03155663512a3fa90c8d4323810468
Guibin Zhang <gzhang@radiumone.com>
2014-10-28 06:41:08 -0700

    Understand why we should restore branch and count

--- libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/string/RestoreIPAddresses.java
+++ libs/lib.algorithm/src/main/java/guibin/zhang/leetcode/string/RestoreIPAddresses.java
@@ -19,30 +19,62 @@ public class RestoreIPAddresses {
     static final int IP_COUNT = 4;
     static final int IP_LEN = 3;
     
+    
+    public List<String> restoreIpAddresses_v4(String s) {
+        
+        List<String> result = new ArrayList<>();
+        if (s.length() < 4 || s.length() > 12) return result;
+        dfs_v4(s, result, "", 0);
+        return result;
+    }
+    
+    public void dfs_v4 (String s, List<String> result, String branch, int count) {
+        
+        if (count == 3 && isValid(s)) {
+            branch += s;
+            result.add(branch);
+            return;
+        }
+        for (int i = 1; i < 4 && s.length() > i; i++) {
+            String segment = s.substring(0, i);
+            if (isValid(segment)) {
+                branch += segment + ".";
+                if (s.length() > 0) {
+                    dfs_v4 (s.substring(i), result, branch, ++count);
+                    //Since branch and count get changed when goes into dfs, so after dfs then should be restored.
+                    branch = branch.substring(0, branch.length() - segment.length() -1);
+                    count--;
+                }
+            }
+        }
+    }
+    
+    
     public List<String> restoreIpAddresses_v3(String s) {
-        ArrayList<String> res = new ArrayList<String>();  
-        if (s.length()<4||s.length()>12) return res;  
-        dfs(s,"",res,0);  
+        
+        List<String> res = new ArrayList<>();  
+        if (s.length() < 4 || s.length()>12) return res;  
+        dfs_v3(s, "", res, 0);  
         return res; 
     }
     
-    public void dfs(String s, String tmp, ArrayList<String> res, int count){  
+    public void dfs_v3(String s, String branch, List<String> res, int count){  
         if (count == 3 && isValid(s)) {  
-            res.add(tmp + s);  
+            res.add(branch + s);  
             return;  
         }  
-        for(int i=1; i<4 && i<s.length(); i++){  
+        for(int i = 1; i < 4 && i < s.length(); i++){  
             String substr = s.substring(0,i);  
             if (isValid(substr)){  
-                dfs(s.substring(i), tmp + substr + '.', res, count+1);  
+                dfs_v3(s.substring(i), branch + substr + '.', res, count+1);  
             }  
         }  
     }  
       
     public boolean isValid(String s){  
-        if (s.charAt(0)=='0') return s.equals("0");  
+        if (s.charAt(0) == '0') return s.equals("0");  
         int num = Integer.parseInt(s);  
-        return num<=255 && num>0;  
+        return num <= 255 && num > 0;  
     }
     
     
